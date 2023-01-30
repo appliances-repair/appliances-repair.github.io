f829542ced41e97de6c30a8f7ec2cb077a5b28b4
Yulia Tsernant <yulia.tsernant@d-d.me>
2023-01-29 17:51:00 -0800

    Added reviews

--- main.js
+++ main.js
@@ -223,3 +223,25 @@ document.addEventListener(
   "keydown",
   (event) => event.key === "Escape" && closeModal()
 );
+
+// let stars = Array.from(document.querySelectorAll(".stars-container i"));
+
+// stars.forEach((element) => {
+//   element.addEventListener("click", (e) => {
+//     rate(element);
+//   });
+//   /********** */
+//   element.addEventListener("mouseover", (e) => {
+//     rate(element);
+//   });
+// });
+
+// function rate(element) {
+//   stars.forEach((el) => {
+//     el.classList.remove("selected");
+//   });
+//   selectedRating = stars.indexOf(element);
+//   for (let i = 0; i <= selectedRating; i++) {
+//     stars[i].classList.add("selected");
+//   }
+// }
